9f7fc01aa9a8370c362eb40e3abb13cb094407de
unknown <san17055@byui.edu>
2019-10-01 22:41:47 -0600

    updated

--- web/functions.php
+++ web/functions.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>"."Why did you click that... when I asked you specifically not to?"."/h1>";
+echo "<h1>"."Why did you click that... when I asked you specifically not to?"."</h1>";
 echo '<img src="carrel.gif" alt="michael scott" style="width:300px;height:400px;">';
 ?>

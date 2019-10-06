dd8d41d7b9bb6140abae170aa29c3140f7de4885
unknown <san17055@byui.edu>
2019-10-05 22:24:27 -0600

    updated

--- web/shopping_cart.php
+++ web/shopping_cart.php
@@ -8,47 +8,44 @@ session_start();
     </head>
 <body>
 <div class = "products">
-<table border = 5 bordercolor = red align = center>
-    <th colspan = 14>ABCD</th>
+<table border = 5 bordercolor = black align = center>
+    <th colspan = 14>Please Fill out Order Form:</th>
     <tr>
-        <th colspan = 4>Name</th>
-        <th colspan = 4>Origin</th>
-        <th colspan = 4>Photo</th>
+        <th colspan = 4>Add:</th>
+        <th colspan = 4>Qty:</th>
+        <th colspan = 4>Product:</th>
     </tr>
     <tr>
-        <td colspan = 4>Bugatti Veyron Super Sport</th>
-        <td colspan = 4>Molsheim, Alsace, France</th>
-        <td colspan = 4><img src="cat_err.gif" alt="" border=3 height=100 width=100></img></td>
+        <td colspan = 4><input type="checkbox" id = "cabbage" name="cabbage" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "cabbage_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="cabbage.JPG" alt="cabbage" border=3 height=100 width=100 align = center></img></td>
     </tr>
     <tr>
-        <td colspan = 4>SSC Ultimate Aero TT  TopSpeed</td>
-        <td colspan = 4>United States</td>
-        <td colspan = 4 border=3 height=100 width=100>Photo1</td>
+        <td colspan = 4><input type="checkbox" id = "carrot" name="carrot" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "carrot_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="carrot.JPG" alt="carrot" border=3 height=100 width=100 align = center></img></td>    </tr>
+    <tr>
+        <td colspan = 4><input type="checkbox" id = "tomato" name="tomato" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "tomato_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="tomato.JPG" alt="tomato" border=3 height=100 width=100 align = center></img></td>    </tr>
+    <tr>
+        <td colspan = 4><input type="checkbox" id = "lemon" name="lemon" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "lemon_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="lemon.JPG" alt="lemon" border=3 height=100 width=100 align = center></img></td>    </tr>
+    <tr>
+        <td colspan = 4><input type="checkbox" id = "avacado" name="avacado" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "avacado_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="avacado.JPG" alt="avacado" border=3 height=100 width=100 align = center></img></td>
     </tr>
     <tr>
-        <td colspan = 4>Koenigsegg CCX</td>
-        <td colspan = 4>Ängelholm, Sweden</td>
-        <td colspan = 4 border=4 height=100 width=300>Photo1</td>
+        <td colspan = 4><input type="checkbox" id = "jalepeno" name="jalepeno" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "jalepeno_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="jalepeno.JPG" alt="jalepeno" border=3 height=100 width=100 align = center></img></td>
     </tr>
     <tr>
-        <td colspan = 4>Saleen S7</td>
-        <td colspan = 4>Irvine, California, United States</td>
-        <td colspan = 4 border=3 height=100 width=100>Photo1</td>
-    </tr>
-    <tr>
-        <td colspan = 4> McLaren F1</td>
-        <td colspan = 4>Surrey, England</td>
-        <td colspan = 4 border=3 height=100 width=100>Photo1</td>
-    </tr>
-    <tr>
-        <td colspan = 4>Ferrari Enzo</td>
-        <td colspan = 4>Maranello, Italy</td>
-        <td colspan = 4 border=3 height=100 width=100>Photo1</td>
-    </tr>
-    <tr>
-        <td colspan = 4> Pagani Zonda F Clubsport</td>
-        <td colspan = 4>Modena, Italy</td>
-        <td colspan = 4 border=3 height=100 width=100>Photo1</td>
+        <td colspan = 4><input type="checkbox" id = "watermelon" name="watermelon" value=""></td>
+        <td colspan = 4>Quantity: <input type = "number" id = "watermelon_qty" min = "1" max = "100"></td>
+        <td colspan = 4><img src="watermelon.JPG" alt="watermelon" border=3 height=100 width=100 align = center></img></td>
     </tr>
 </table>
  </body>

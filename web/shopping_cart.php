18c304cf3d8548a2bbf460cdd892cf50a542c5d9
unknown <san17055@byui.edu>
2019-10-05 22:26:50 -0600

    updated

--- web/shopping_cart.php
+++ web/shopping_cart.php
@@ -23,29 +23,29 @@ session_start();
     <tr>
         <td colspan = 4><input type="checkbox" id = "carrot" name="carrot" value=""></td>
         <td colspan = 4>Quantity: <input type = "number" id = "carrot_qty" min = "1" max = "100"></td>
-        <td colspan = 4><img src="carrot.JPG" alt="carrot" border=3 height=100 width=100 align = center></img></td>    </tr>
+        <td colspan = 4><img src="carrot.jpg" alt="carrot" border=3 height=100 width=100 align = center></img></td>    </tr>
     <tr>
         <td colspan = 4><input type="checkbox" id = "tomato" name="tomato" value=""></td>
         <td colspan = 4>Quantity: <input type = "number" id = "tomato_qty" min = "1" max = "100"></td>
-        <td colspan = 4><img src="tomato.JPG" alt="tomato" border=3 height=100 width=100 align = center></img></td>    </tr>
+        <td colspan = 4><img src="tomato.jpg" alt="tomato" border=3 height=100 width=100 align = center></img></td>    </tr>
     <tr>
         <td colspan = 4><input type="checkbox" id = "lemon" name="lemon" value=""></td>
         <td colspan = 4>Quantity: <input type = "number" id = "lemon_qty" min = "1" max = "100"></td>
-        <td colspan = 4><img src="lemon.JPG" alt="lemon" border=3 height=100 width=100 align = center></img></td>    </tr>
+        <td colspan = 4><img src="lemon.jpg" alt="lemon" border=3 height=100 width=100 align = center></img></td>    </tr>
     <tr>
         <td colspan = 4><input type="checkbox" id = "avacado" name="avacado" value=""></td>
         <td colspan = 4>Quantity: <input type = "number" id = "avacado_qty" min = "1" max = "100"></td>
-        <td colspan = 4><img src="avacado.JPG" alt="avacado" border=3 height=100 width=100 align = center></img></td>
+        <td colspan = 4><img src="avacado.jpg" alt="avacado" border=3 height=100 width=100 align = center></img></td>
     </tr>
     <tr>
         <td colspan = 4><input type="checkbox" id = "jalepeno" name="jalepeno" value=""></td>
         <td colspan = 4>Quantity: <input type = "number" id = "jalepeno_qty" min = "1" max = "100"></td>
-        <td colspan = 4><img src="jalepeno.JPG" alt="jalepeno" border=3 height=100 width=100 align = center></img></td>
+        <td colspan = 4><img src="jalepeno.jpg" alt="jalepeno" border=3 height=100 width=100 align = center></img></td>
     </tr>
     <tr>
         <td colspan = 4><input type="checkbox" id = "watermelon" name="watermelon" value=""></td>
         <td colspan = 4>Quantity: <input type = "number" id = "watermelon_qty" min = "1" max = "100"></td>
-        <td colspan = 4><img src="watermelon.JPG" alt="watermelon" border=3 height=100 width=100 align = center></img></td>
+        <td colspan = 4><img src="watermelon.jpg" alt="watermelon" border=3 height=100 width=100 align = center></img></td>
     </tr>
 </table>
  </body>

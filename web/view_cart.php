4fe4ed2c25e17478c21dadc7a3e87daf00b87972
unknown <san17055@byui.edu>
2019-10-06 00:28:33 -0600

    updated

--- web/view_cart.php
+++ web/view_cart.php
@@ -1,6 +1,6 @@
 <?php
     if(isset($_SESSION['cabbage_qty'])){
-        $num_cabbage = (int)$_SESSION['cabbage_qty'];
+        $num_cabbage = (int)$_GET['cabbage_qty'];
         $price = 8*$num_cabbage;
         echo "<tr><td>Cabbage(s): ".$num_cabbage."lbs<br>
                 $".$price.".00</td></tr>";

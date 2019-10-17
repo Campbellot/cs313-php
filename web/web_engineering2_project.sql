96ae5e9ad1d135a49bd7745043e1729b8d6de583
unknown <san17055@byui.edu>
2019-10-16 23:05:03 -0600

    sqlfile

---
+++ web/web_engineering2_project.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE rock_paper_scissors;
+
+
+  CREATE TABLE results (
+    game_id SERIAL PRIMARY KEY,
+    win BOOLEAN,
+    loss BOOLEAN,
+    input VARCHAR(9) NOT NULL  
+    );
+
+    CREATE TABLE wins (
+    game_id INT REFERENCES results(game_id),
+    win BOOLEAN,
+    rock BOOLEAN NOT NULL,
+    paper BOOLEAN NOT NULL,
+    scissors BOOLEAN NOT NULL
+  );
+
+    CREATE TABLE losses (
+    game_id INT REFERENCES results(game_id),
+    loss BOOLEAN,
+    rock BOOLEAN NOT NULL,
+    paper BOOLEAN NOT NULL,
+    scissors BOOLEAN NOT NULL
+  );

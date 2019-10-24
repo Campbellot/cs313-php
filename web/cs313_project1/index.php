ba35db4a84a57a634b25a385a13b0e9dbf9001fb
unknown <san17055@byui.edu>
2019-10-24 16:15:10 -0600

    test1

--- web/cs313_project1/index.php
+++ web/cs313_project1/index.php
@@ -13,28 +13,50 @@ require("dbConnect.php");
 $db = get_db();
 // var_dump($db);exit;
 
-try
-{
-	// Notice that we do not use "SELECT *" here. It is best practice
-	// to only bring back the fields that you need.
-	// prepare the statement
-	$statement = $db->prepare('SELECT game_id, win, loss, input FROM results');
-	$statement->execute();
-    // Go through each result
+// try
+// {
+// 	// Notice that we do not use "SELECT *" here. It is best practice
+// 	// to only bring back the fields that you need.
+// 	// prepare the statement
+// 	$statement = $db->prepare('SELECT game_id, win, loss, input FROM results');
+// 	$statement->execute();
+//     // Go through each result
     
-	while ($row = $statement->fetch())
-	{
-        echo $row['game_id']."\n";
-        echo (($row['win']) ? "TRUE" : "FALSE")."\n";
-        echo (($row['loss']) ? "TRUE" : "FALSE")."\n";
-        echo $row['input']."\n";
-	}
-}
-catch (PDOException $ex)
-{
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
-	echo "Error connecting to DB. Details: $ex";
-	die();
-}
+// 	while ($row = $statement->fetch())
+// 	{
+//         echo $row['game_id']."\n";
+//         echo (($row['win']) ? "TRUE" : "FALSE")."\n";
+//         echo (($row['loss']) ? "TRUE" : "FALSE")."\n";
+//         echo $row['input']."\n";
+// 	}
+// }
+// catch (PDOException $ex)
+// {
+// 	// Please be aware that you don't want to output the Exception message in
+// 	// a production environment
+// 	echo "Error connecting to DB. Details: $ex";
+// 	die();
+// }
 ?>
+<!DOCTYPE HTML>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Rock, Paper, Scissors</title>
+
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+  <div class="flex-container">
+    <div><h1>Select Your Choice:</h1></div>
+  <div><img src="images/rock.jpg" alt="rock"></div>
+  <div><img src="images/paper.jpg" alt="paper"></div>
+  <div><img src="images/scissors.jpg" alt="scissors"></div>
+</div>
+</body>
+</html>

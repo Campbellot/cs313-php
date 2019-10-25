c5f9dd98267309306511dc455e9060549b5a9dcd
unknown <san17055@byui.edu>
2019-10-25 17:22:29 -0600

    test2

--- web/cs313_project1/dataRequest.php
+++ web/cs313_project1/dataRequest.php
@@ -1,4 +1,38 @@
 <?php
+
+
+require("dbConnect.php");
+$db = get_db();
+// var_dump($db);exit;
+$result = array();
+$i = 0;
+try
+{
+	// Notice that we do not use "SELECT *" here. It is best practice
+	// to only bring back the fields that you need.
+	// prepare the statement
+	$statement = $db->prepare('SELECT game_id, win, loss, input FROM results');
+	$statement->execute();
+    // Go through each result
+    
+	while ($row = $statement->fetch())
+	{
+        $result[$i] = (object)(
+            "game_id" => $row['game_id'];
+            "win" => (($row['win']) ? TRUE : FALSE);
+            "loss" => (($row['loss']) ? TRUE : FALSE);
+            "input" => $row['input']);
+        $i++;
+	}
+}
+catch (PDOException $ex)
+{
+	// Please be aware that you don't want to output the Exception message in
+	// a production environment
+	echo "Error connecting to DB. Details: $ex";
+	die();
+}
+var_dump($result);exit;
 $stat = $_GET['stat'];
 switch($stat){
     case "paper":

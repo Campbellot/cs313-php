92b9954d197dac1e557c36ce288fd6e76ea6bc85
unknown <san17055@byui.edu>
2019-10-25 17:36:23 -0600

    test2

--- web/cs313_project1/dataRequest.php
+++ web/cs313_project1/dataRequest.php
@@ -31,18 +31,18 @@ catch (PDOException $ex)
 	echo "Error connecting to DB. Details: $ex";
 	die();
 }
-var_dump($result);
-$stat = $_GET['stat'];
-switch($stat){
-    case "paper":
-        echo "<h3>paper statistics blah blah blah\n</h3>";
-    break;
-    case "scissors":
-        echo "<h3>scissors statistics blah blah blah\n</h3>";
-    break;
-    case "rock":
-        echo "<h3>rock statistics blah blah blah\n</h3>";
-    break;
-}
+// var_dump($result);
+// $stat = $_GET['stat'];
+// switch($stat){
+//     case "paper":
+//         echo "<h3>paper statistics blah blah blah\n</h3>";
+//     break;
+//     case "scissors":
+//         echo "<h3>scissors statistics blah blah blah\n</h3>";
+//     break;
+//     case "rock":
+//         echo "<h3>rock statistics blah blah blah\n</h3>";
+//     break;
+// }
 
 ?>

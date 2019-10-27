246eb78e72a716bb6105d8137e2eb0131f5b1834
unknown <san17055@byui.edu>
2019-10-26 19:39:10 -0600

    test3

--- web/cs313_project1/index.php
+++ web/cs313_project1/index.php
@@ -1,39 +1,35 @@
 <?php
-/**********************************************************
-* File: topicEntry.php
-* Author: Br. Burton
-* 
-* Description: This is the PHP file that the user starts with.
-*   It has a form to enter a new scripture and topic.
-*   It posts to the insertTopic.php page.
-***********************************************************/
-// The DB connection logic is in another file so it can be included
-// in each of our different PHP files.
+
 require("dbConnect.php");
+
 $db = get_db();
-// var_dump($db);exit;
+
+$game_id = "";
+$win = "";
+$loss = "";
+$input = "";
+$result = array();
+$i = 0;
 
 try
 {
-	// Notice that we do not use "SELECT *" here. It is best practice
-	// to only bring back the fields that you need.
-	// prepare the statement
-	$statement = $db->prepare('SELECT game_id, win, loss, input FROM results');
+
+  $statement = $db->prepare('SELECT game_id, win, loss, input FROM results');
 	$statement->execute();
-    // Go through each result
-    
+
+  
 	while ($row = $statement->fetch())
 	{
-        echo $row['game_id']."\n";
-        echo (($row['win']) ? "TRUE" : "FALSE")."\n";
-        echo (($row['loss']) ? "TRUE" : "FALSE")."\n";
-        echo $row['input']."\n";
+    $result[$i] = (object)array(
+        "game_id" => $row['game_id'];
+        "win" => (($row['win']) ? TRUE : FALSE);
+        "loss" => (($row['loss']) ? TRUE : FALSE);
+        "input" => $row['input']);
 	}
 }
 catch (PDOException $ex)
 {
-	// Please be aware that you don't want to output the Exception message in
-	// a production environment
 	echo "Error connecting to DB. Details: $ex";
 	die();
 }
+var_dump($result);
